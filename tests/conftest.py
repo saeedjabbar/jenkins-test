abfe0f5e4b0985a3cfd792d8ec6eb7b9c439bad0
Saeed Jabbar <saeedryanjabbar@gmail.com>
2023-07-22 21:55:08 -0400

    updated confest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from pages.registration.login_page import LoginPage
-
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_teardown_method():
@@ -9,29 +7,16 @@ def setup_teardown_method():
     yield
     print("Method level teardown")
 
-
-# @pytest.fixture(scope="module", autouse=True)
-# def setup_teardown_module(request, browser):
-#     print("Module level setup")
-#     if browser == "chrome":
-#         driver = webdriver.Chrome()
-#     if browser == "firefox":
-#         driver = webdriver.Firefox()
-#     driver.implicitly_wait(3)
-#     driver.maximize_window()
-#     driver.get("https://www.letskodeit.com/")
-#     request.module.driver = driver
-#     yield driver
-#     print("Module level teardown")
-#     driver.quit()
-
 @pytest.fixture(scope="class", autouse=True)
 def setup_teardown_class(request, browser):
     print("Class level setup")
     if browser == "chrome":
         driver = webdriver.Chrome()
-    if browser == "firefox":
+    elif browser == "firefox":
         driver = webdriver.Firefox()
+    else:
+        raise ValueError(f"Invalid browser option: {browser}")
+        
     driver.implicitly_wait(3)
     driver.maximize_window()
     driver.get("https://www.letskodeit.com/")
@@ -40,37 +25,16 @@ def setup_teardown_class(request, browser):
     print("Class level teardown")
     driver.quit()
 
-
 def pytest_addoption(parser):
-    parser.addoption("--browser")
-    parser.addoption("--os_type")
+    parser.addoption("--browser", action="store", default="chrome",
+                     help="Browser to be used for testing (chrome/firefox)")
+    parser.addoption("--os_type", action="store", default="mac",
+                     help="Operating system type for testing (mac/windows/linux)")
 
 @pytest.fixture(scope="session")
 def browser(request):
     return request.config.getoption("--browser")
 
-
 @pytest.fixture(scope="session")
 def os_type(request):
     return request.config.getoption("--os_type")
-
-"""
-1. Framework Refactor - Done
-2. Add more methods to custom driver - Done
-3. BasePage Concept - Done
-4. Util Concept - Done
-5. Verifying test result - Done
-6. Convert test modules to test class - Done
-7. Adding logging to framework - Done
-8. Data Driven Testing - Done
-9. How to run complete automation suite - Done
-10. Taking screenshots on failure - Done
-11. Utility for assertion (Framework Refactor) - Done
-12. Reporting and attaching screenshots to reports - Done
-13. GitHub < 1 class- Done
-14. Push the framework code to GitHub
-15. Jenkins < 1 class (CI / CD) - CircleCI, Bamboo, TeamCity
-16. API Testing >= 1 class
-17. Performance Testing >=1
-18. SQL Testing >=1
-"""
